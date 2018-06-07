a5380b7b3419db4cfb94d7fa70843ba181e9a4c4
Janwillem Swalens <jswalens@vub.ac.be>
2018-06-07 16:44:04 +0200

    Update lein project to work accept Clojure and Java code.

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject chocola "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A unified framework of futures, transactions, and actors."
+  :url "http://soft.vub.ac.be/~jswalens/chocola/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  ; Documentation about mixing Clojure and Java in a Leiningen project at
+  ; https://github.com/technomancy/leiningen/blob/master/doc/MIXED_PROJECTS.md
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"])
