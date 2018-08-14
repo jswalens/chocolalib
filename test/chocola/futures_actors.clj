5a6f201838d6fcd4719498ee190b97e5f0b17587
Janwillem Swalens <jswalens@vub.ac.be>
2018-08-14 14:41:41 +0200

    Add test exposing the "escaping task problem".

---
+++ test/chocola/futures_actors.clj
@@ -0,0 +1,28 @@
+(ns chocola.futures-actors
+  (:require [clojure.test :refer :all]
+            [chocola.core]))
+
+(deftest escaping-task-problem
+  ; sender sends 50 messages (containing their number) to receiver. We expect
+  ; them to be received in consecutive order.
+  (let [n-msgs      50
+        received-is (atom [])
+        done?       (promise)
+        receiver-beh
+          (behavior [] [i]
+            ;(println "Got" i)
+            (swap! received-is conj i)
+            (if (= i (- n-msgs 1))
+              (deliver done? true)))
+        receiver
+          (spawn receiver-beh)
+        sender-beh
+          (behavior [] [i]
+            (future (Thread/sleep (rand-int 200)) (send receiver i)))
+        sender
+          (spawn sender-beh)]
+    (dotimes [i n-msgs]
+      ;(println "Send" i)
+      (send sender i))
+    @done?
+    (is (= @received-is (range n-msgs)))))
