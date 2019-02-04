797cd0c35d19171c8927f3a595350fd2b9440f75
Janwillem Swalens <jswalens@vub.ac.be>
2019-02-04 14:09:10 +0100

    Split code in test over multiple lines to make it easier to debug.

--- test/chocola/transactions_misc.clj
+++ test/chocola/transactions_misc.clj
@@ -8,7 +8,8 @@
 (def r (ref 0))
 
 (deftest io []
-  (dosync (ref-set r 0))
+  (dosync
+    (ref-set r 0))
   (is (thrown? java.lang.IllegalStateException
     (dosync
       (ref-set r 1)
@@ -18,10 +19,14 @@
   (is (= @r 0))); threw so no change
 
 (deftest io-tx-fut-1 []
-  (dosync (ref-set r 0))
+  (dosync
+    (ref-set r 0))
   (dosync
     (ref-set r 1)
-    (let [f (future (ref-set r 2) (is (thrown? java.lang.IllegalStateException (io! (is false)))))]
+    (let [f (future
+              (ref-set r 2)
+              (is (thrown? java.lang.IllegalStateException
+                           (io! (is false)))))]
       @f)) ; does not throw as it was caught internally
   (is (= @r 2))) ; caught so last r is valid
 
@@ -32,7 +37,9 @@
     ; already caught internally. Is this expected behavior?
     (dosync
       (ref-set r 1)
-      (let [f (future (ref-set r 2) (io! (is false)))]
+      (let [f (future
+                (ref-set r 2)
+                (io! (is false)))]
         (is (thrown? java.util.concurrent.ExecutionException @f))
         ; throws ExecutionException that wraps IllegalStateException in future
         (ref-set r 3)
@@ -40,13 +47,16 @@
   (is (= @r 0))); threw so no change
 
 (deftest io-tx-fut-3 []
-  (dosync (ref-set r 0))
+  (dosync
+    (ref-set r 0))
   (is (thrown? java.util.concurrent.ExecutionException
     ; The future is joined implicitly, at which point its exception bubbles up.
     ; ExecutionException wraps IllegalStateException in future.
     (dosync
       (ref-set r 1)
-      (future (ref-set r 2) (io! (is false))))))
+      (future
+        (ref-set r 2)
+        (io! (is false))))))
   (is (= @r 0))) ; threw so no change
 
 ; === send to Agent ===
