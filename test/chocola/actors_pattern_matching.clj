033bbefacf287d8a21f644d632a8283dbd6a893a
Janwillem Swalens <jswalens@vub.be>
2019-05-08 15:47:56 +0200

    Test scoping rules, shadowing and matching.

--- test/chocola/actors_pattern_matching.clj
+++ test/chocola/actors_pattern_matching.clj
@@ -64,5 +64,24 @@
                      [:one-param 8]
                      [:symbol :hello]]))))
 
-; TODO: test whether internal memory is still accessible + what if a pattern
-; shadows an internal variable (or external even?)
+(deftest scoping
+  ;                     1234-678
+  ; scope outside actor xxxx....
+  ; behavior parameters .x.x.x.x
+  ; message parameters  ..xx..xx
+  (let [p1 (promise)
+        p2 (promise)
+        x1 1
+        x2 1
+        x3 1
+        x4 1
+        b (behavior [x2 x4 x6 x8]
+            [p x3 x4 x7 x8] (deliver p [:match [x1 x2 x3 x4 0 x6 x7 x8]])
+            [p y3 y4 y7 y8] (deliver p [:else  [x1 x2 x3 x4 0 x6 0  x8] [y3 y4 y7 y8]]))
+        a (spawn b 2 2 2 2)]
+    (send a p1 3 3 3 3)
+    (is (= (deref p1 3000 false) [:else [1 2 1 2 0 2 0 2] [3 3 3 3]]))
+    (is (realized? p1) "Promise not delivered after 3000 ms.")
+    (send a p2 1 2 3 2)
+    (is (= (deref p2 3000 false) [:match [1 2 1 2 0 2 3 2]]))
+    (is (realized? p2) "Promise not delivered after 3000 ms.")))
