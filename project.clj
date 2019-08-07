446c87ec991fe1382ecf824ec42de2d5e053a606
Janwillem Swalens <jswalens@vub.be>
2019-08-07 11:22:18 +0200

    Depend on latest version of Clojure (1.10.1), and thus this version will be included in the uberjar.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.match "0.3.0"]]
 
   :injections [(require 'chocola.core)]
